fc50cd47e5824c2c581b7a9faa638a7e86804dcb
Andreas Biørn-Hansen <andreas@flytit.no>
2022-08-31 09:22:02 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Baal_2022_forelesning_1',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
